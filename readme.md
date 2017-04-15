c91bdfc7bdd0258571a4f92216c0916ac0f97bf2
ia-lex <girtor@gmail.com>
2017-04-15 15:43:51 -0500

    A few fixes was added

--- readme.md
+++ readme.md
@@ -1,2 +1,16 @@
 # clickdelivery-user-manager
 Clickdelivery-user-manager-test
+
+This application is an user manager, it has a normal login system using laravel's auth system, facebook authentication using Socialite package and a system based in roles, because we are using laravel auth system the order of the data in the roles table Must be like this:
+
+| id| name | alias |
+|---| ------ | ------ |
+| 1 | Administrator | ADM |
+| 2 | Agent | AGT |
+| 3 | User | USR |
+
+This ensures a proper execution of the system.
+
+# You should now
+- When a new user is been registered by himself, his/her account must be activated by an administrator
+- When a new user is been registered by using facebook, the account is automatically activated
--- resources/views/auth/manager-update-modal.blade.php
+++ resources/views/auth/manager-update-modal.blade.php
@@ -7,9 +7,7 @@
         <button type="button" class="close" data-dismiss="modal">&times;</button>
         <h4 class="modal-title">User Data</h4>
       </div>
-      <div class="modal-body">     
-
-        
+      <div class="modal-body">        
             <div class="row">
                 <div class="col-md-10 col-md-offset-1">
                     <div class="panel panel-default">
@@ -80,13 +78,11 @@
                                     <label for="role_id" class="col-md-4 control-label">Role</label>
 
                                     <div class="col-md-6">
-                                        <input id="role_id" type="number" class="form-control" name="role_id">
-
-                                        @if($errors->has('role_id'))
-                                            <span class="help-block">
-                                                <strong>{{ $errors->first('role_id') }}</strong>
-                                            </span>
-                                        @endif
+                                        <select id="role_id" class="form-control" name="role_id">
+                                            <option value="1">Administrator</option>
+                                            <option value="2">Agent</option>
+                                            <option value="3">User</option>
+                                        </select>
                                     </div>
                                 </div>
 
@@ -129,8 +125,8 @@
                                             Update
                                         </button>
                                     </div>
-                                </div>
-                            </form>
+                                </div>                                
+                            </form>                            
                         </div>
                     </div>
                 </div>
@@ -144,83 +140,8 @@
 
   </div>
 </div>
-
-<!--
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">User Data</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="PUT" action="{{ route('update') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('phone_number') ? 'has-error' : ''}}">
-                            <label for="phone_number" class="col-md-4 control-label">Phone number</label>
-
-                            <div class="col-md-6">
-                                <input id="phone_number" type="number" class="form-control" name="phone_number">
-
-                                @if($errors->has('phone_number'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('phone_number') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->
+<script type="text/javascript">
+    @if(count($errors) > 0)
+        //showModal();
+    @endif
+</script>
